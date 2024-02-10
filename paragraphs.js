bdf96527109f38ba00f7e8bf112888439686db06
james sajan <97374957+Jamesupback@users.noreply.github.com>
2024-02-10 23:01:23 +0530

    Add files via upload

---
+++ paragraphs.js
@@ -0,0 +1,67 @@
+const noob = [
+    "Authors often misinterpret the lettuce as a folklore rabbi, when in actuality it feels more like an uncursed bacon. Pursued distances show us how mother-in-laws can be charleses. Authors often misinterpret the lion as a cormous science, when in actuality it feels more like a leprous lasagna. Recent controversy aside, their band was, in this moment, a racemed suit. The clutch of a joke becomes a togaed chair. The first pickled chess is.",
+    "In modern times the first scrawny kitten is, in its own way, an input. An ostrich is the beginner of a roast. An appressed exhaust is a gun of the mind. A recorder is a grade from the right perspective. A hygienic is the cowbell of a skin. Few can name a dun brazil that isn't a highbrow playroom. The unwished beast comes from a thorny oxygen. An insured advantage's respect comes with it the thought that the lucid specialist is a fix.",
+    "In ancient times the legs could be said to resemble stroppy vegetables. We can assume that any instance of a centimeter can be construed as an enate paste. One cannot separate pairs from astute managers. Those americas are nothing more than fish. If this was somewhat unclear, authors often misinterpret the gosling as an unfelt banjo, when in actuality it feels more like a professed galley. A bow of the squirrel is assumed.",
+    "What we don't know for sure is whether or not a pig of the coast is assumed to be a hardback pilot. The literature would have us believe that a dusky clave is not but an objective. Few can name a limbate leo that isn't a sunlit silver. The bow is a mitten. However, the drawer is a bay. If this was somewhat unclear, few can name a paunchy blue that isn't a conoid bow. The undrunk railway reveals itself as a downstage bamboo to those who look.",
+    "Their politician was, in this moment, a notour paperback. The first armless grouse is, in its own way, a gear. The coat is a wash. However, a cake is the llama of a caravan. Snakelike armies show us how playgrounds can be viscoses. Framed in a different way, they were lost without the fatal dogsled that composed their waitress. Far from the truth, the cockney freezer reveals itself as a wiggly tornado to those who look. The first hawklike sack.",
+    "An aunt is a bassoon from the right perspective. As far as we can estimate, some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals itself as a sclerous llama to those who look. A hip is the skirt of a peak. Some hempy laundries are thought of simply as orchids. A gum is a trumpet from the right perspective. A freebie flight is a wrench of the mind. Some posit the croupy.",
+    "A baby is a shingle from the right perspective. Before defenses, collars were only operations. Bails are gleesome relatives. An alloy is a streetcar's debt. A fighter of the scarecrow is assumed to be a leisured laundry. A stamp can hardly be considered a peddling payment without also being a crocodile. A skill is a meteorology's fan. Their scent was, in this moment, a hidden feeling. The competitor of a bacon becomes a boxlike cougar.",
+    "A broadband jam is a network of the mind. One cannot separate chickens from glowing periods. A production is a faucet from the right perspective. The lines could be said to resemble zincoid females. A deborah is a tractor's whale. Cod are elite japans. Some posit the wiglike norwegian to be less than plashy. A pennoned windchime's burst comes with it the thought that the printed trombone is a supply. Relations are restless tests.",
+    "In recent years, some teeming herons are thought of simply as numbers. Nowhere is it disputed that an unlaid fur is a marble of the mind. Far from the truth, few can name a glossy lier that isn't an ingrate bone. The chicken is a giraffe. They were lost without the abscessed leek that composed their fowl. An interviewer is a tussal bomb. Vanward maracas show us how scarfs can be doubts. Few can name an unguled punch that isn't pig.",
+    "A cough is a talk from the right perspective. A designed tractor's tray comes with it the thought that the snuffly flax is a rainbow. Their health was, in this moment, an earthy passbook. This could be, or perhaps the swordfishes could be said to resemble healthy sessions. A capricorn is a helium from the right perspective. However, a sled is a mailman's tennis. The competitor of an alarm becomes a toeless raincoat. Their twist was, in this moment.",
+    "Authors often misinterpret the flag as a wayless trigonometry, when in actuality it feels more like a bousy gold. Few can name a jasp oven that isn't a stutter grape. They were lost without the huffy religion that composed their booklet. Those waves are nothing more than pedestrians. Few can name a quartered semicolon that isn't a rounding scooter. Though we assume the latter, the literature would have us believe.",
+    "This could be, or perhaps few can name a pasteboard quiver that isn't a brittle alligator. A swordfish is a death's numeric. Authors often misinterpret the mist as a swelling asphalt, when in actuality it feels more like a crosswise closet. Some posit the tonal brother-in-law to be less than newborn. We know that the sizes could be said to resemble sleepwalk cycles. Before seasons, supplies were only fighters. Their stew was, in this moment.",
+    "The vision of an attempt becomes a lawny output. Dibbles are mis womens. The olden penalty reveals itself as a bustled field to those who look. Few can name a chalky force that isn't a primate literature. However, they were lost without the gamy screen that composed their beret. Nowhere is it disputed that a step-uncle is a factory from the right perspective. One cannot separate paints from dreary windows. What we don't know for sure is whether.",
+    "A tramp is a siamese from the right perspective. We know that a flitting monkey's jaw comes with it the thought that the submersed break is a pamphlet. Their cream was, in this moment, a seedy daffodil. The nest is a visitor. Far from the truth, they were lost without the released linen that composed their step-sister. A vibraphone can hardly be considered a pardine process without also being an archaeology. The bay of a hyacinth becomes.",
+    "The frosts could be said to resemble backstage chards. One cannot separate colleges from pinkish bacons. Far from the truth, the mom of a rooster becomes a chordal hydrogen. A tempo can hardly be considered a purer credit without also being a pajama. The first combined ease is, in its own way, a pantyhose. Extending this logic, the guides could be said to resemble reddest monkeies. Framed in a different way, an addle hemp is a van.",
+    "Far from the truth, an ajar reminder without catamarans is truly a foundation of smarmy semicircles. An alike board without harps is truly a satin of fated pans. A hubcap sees a parent as a painful beautician. The zeitgeist contends that some intense twigs are thought of simply as effects. A cross is a poppied tune. The valanced list reveals itself as an exchanged wrist to those who look. Recent controversy aside.",
+    "The hefty opinion reveals itself as a sterile peer-to-peer to those who look. This could be, or perhaps the watch of a diamond becomes a bosom baboon. In recent years, some posit the unstuffed road to be less than altern. It's an undeniable fact, really; the livelong lettuce reveals itself as an unstuffed soda to those who look. In ancient times a bit is a balance's season. The popcorn of a morning becomes a moonless beauty.",
+    "If this was somewhat unclear, a friend is a fridge from the right perspective. An upset carriage is a stitch of the mind. To be more specific, a temper is a pair from the right perspective. Authors often misinterpret the liquid as a notchy baseball, when in actuality it feels more like an unbarbed angle. Though we assume the latter, the first vagrom report is, in its own way, a tower. We know that the octopus of a cd becomes an unrent dahlia.",
+    "A reptant discussion's rest comes with it the thought that the condemned syrup is a wish. The drake of a wallaby becomes a sonant harp. If this was somewhat unclear, spotty children show us how technicians can be jumps. Their honey was, in this moment, an intime direction. A ship is the lion of a hate. They were lost without the croupous jeep that composed their lily. In modern times a butcher of the birth is assumed to be a spiral bean.",
+    "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless toy. A board is the cast of a religion. In ancient times the first stinko sailboat is, in its own way, an exchange. Few can name a tutti channel that isn't a footless operation. Extending this logic, an oatmeal is the rooster of a shake. Those step-sons are nothing more than matches."
+];
+
+const beginner=[
+    `Significant increase in psychiatric morbidity in
+    the disaster affected group (tables 1 and 2). Excess
+    morbidity was observed in both the genders in the disaster
+    affected group. Overall prevalence of 139 per thousand
+    in the affected sample was significantly higher than 68
+    per thousand in the control sample. 21.46% adult males
+    in the affected group received psychiatric diagnosis
+    compared to 13.14% in the control group. Corresponding
+    figures for adult females were 14.99% and 5.05%; for
+    male children 7.67% and 4.26%; and for female children
+    6.70% and 3.17% respectively. Differential distribution
+    of cases was noticed in the affected villages paralleling
+    the gradient of disaster losses. There was a clustering of
+    cases within families`,
+
+    `278 children were identified as having psychiatric
+    disorder in study sample of 2064 children in age –
+    range (0-16) years giving prevalence rate of 13.4%.
+    It was found that 41 (2.0%) children had diagnosis
+    only on axis II (developmental delay), 14(0.7%) had
+    diagnosis on axis III (intellectual level), 142 (6.9%)
+    had one diagnosis on axis I (psychiatric disorder),
+    44(2.1%) had more than one diagnosis on axis I,
+    and 37 children (1.8%) had diagnosis on more than
+    one axes of the multi axial diagnostic classification.`,
+
+    `Nocturnal enuresis was the commonest diagnosis (96,
+        4.16%) followed by pica (55, 2.38%)conduct disorder
+        (41, 1.78%) specific developmental disorders
+        (29,1.26%), mental retardation (26, 1.13%) and
+        stuttering (19, 0.62%) dissociative disorder was the
+        next in order (4,0.17%) 28 (1.2%) children received
+        a diagnosis unspecified mental disorder as a second
+        or third diagnosis.`,
+]
+
+pro = [
+    "In the fantastical land of 7XyZ!@# where dragons soar and 2AaSsDdFf creatures roam, the 4QwErTyUioP lion dances with 9JkLlMmNnGgHh. Meanwhile, 6ZxCcVvBbNn 3MmMnBb9IiKkLl plays tunes on 8KkLlOoPp7UuYy the magical xylophone while 0PpOo9i8u7yUu the moon whispers secrets to the stars.",
+    "PoiuYtReWq 4TtYyUuIiOpP 3SsDdFfGgHhJj 6KkLlMmNnBb is a realm filled with 9ZzXxCcVvBbNnMm wonders and 8JjHhGgFfDdSs enchantments. In this land, 7QqWwEeRrTt creatures of 2AaSsDdFfGgHhJj 1ZzXxCcVvBbNn roam freely, while 0PpOoIiUuYyTtYy the rivers sing melodies of 5QqWwEeRrTt love and 4PpOoIiUuYy the trees whisper ancient tales.",
+    "AsDfGhJkL; 7TtYyUuIiOpP is a magical 8JjHhGgFfDdSs world where 6AaSsDdFfGg creatures of 4ZzXxCcVvBbNnMm roam freely. In the 9KkLlMmNnBb 3QqWwEeRrTt lands of 1PpOoIiUuYy, 0ZzXxCcVvBbNnMm dragons soar high in the 5FfGgHhJjKkLl skies while 2XxCcVvBbNnMmPpOo8iI7uU9yYy the forests hum with 3SsDdFfGg enchanting melodies.",
+    "QwErTyUiO1p 3aAsDdFfGgHhJj 7KkLlMmNnBbVvC is a land where 8JjHhGgFfDdSs creatures of 2zZxXcCvVbBnNmMmMm roam freely among 6mMnNkKlL5jJ8hHgGfF7dD. In the 9jJkKlL5lL6 world of 4sSdDfFgG, 0pPoOiI9u8yY7tT6rRtYy the moon dances with 1eEwWqQaAsS enchanting delight while 5gGhHjJkKlL the stars whisper secrets to the 4rRtTyYuUyYy skies.",
+    "1QqWwEeRrTtYyUuIi is a magical 2OoPpIiUuYy land filled with 3HhGgFfDdSsAa creatures of 4NnMmBbVvCcXxZz wonders. In this realm, 5JjKkLlMmNnBbVvC creatures roam freely, while 6QqWwEeRrTtYyUu the rivers sing melodies of 7IiOoPpUuYyTt love and 8RrTtYyUuIiOo the trees whisper ancient tales to the 9PpOoIiUuYyTtYy skies."
+]
---
+++ script.js
@@ -0,0 +1,118 @@
+const typingText = document.querySelector(".typing-text p"),
+inpField = document.querySelector(".wrapper .input-field"),
+tryAgainBtn = document.querySelector(".content button"),
+timeTag = document.querySelector(".time span b"),
+mistakeTag = document.querySelector(".mistake span"),
+wpmTag = document.querySelector(".wpm span"),
+cpmTag = document.querySelector(".cpm span");
+
+welcometab=document.getElementById("welcome");
+startbutton=document.getElementById('welcome-button');
+wrapper=document.getElementById('wrap')
+endscreen=document.getElementById("endscreen")
+
+
+let username,classid;
+let timer,
+maxTime = 5,
+timeLeft = maxTime,
+charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const selection=document.getElementById("dropdown").value
+    const ranIndex = Math.floor(Math.random() * eval(selection).length);
+    typingText.innerHTML = "";
+    eval(selection)[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let wpm;
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if(charIndex < characters.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if(typedChar == null) {
+            if(charIndex > 0) {
+                charIndex--;
+                if(characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if(characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        wpm = Math.round((((charIndex - mistakes)  / 5) / (maxTime - timeLeft)) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        wpmTag.innerText =wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        endscreen.style.display='flex';
+        wrapper.style.display='none'
+        wpm = Math.round((((charIndex - mistakes)  / 5) / (maxTime - timeLeft)) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        document.getElementById("uid").innerText=username
+        document.getElementById("classid").innerText=classid
+        document.getElementById("cpmid").innerText=charIndex-mistakes
+        document.getElementById('missid').innerText=mistakes
+        document.getElementById("wordsper").innerText=wpm
+        clearInterval(timer);
+        inpField.value = "";
+    }   
+}
+
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round((((charIndex - mistakes)  / 5) / (maxTime - timeLeft)) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+document.getElementById("inputform").addEventListener("submit",(event)=>{
+    username=document.getElementById('username').value;
+    classid=document.getElementById('userclass').value;
+    console.log(username,classid)
+    event.preventDefault();
+    welcometab.style.display='none';
+    wrapper.style.display='block';
+    loadParagraph();
+    inpField.addEventListener("input", initTyping);
+})
+
+
+//tryAgainBtn.addEventListener("click", resetGame);
